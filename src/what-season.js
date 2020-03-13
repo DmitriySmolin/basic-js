b4f795ec27973eec69b5121981c2d9049432b8e5
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-03-13 14:06:30 +0400

    feat: add solution for task "what-season"

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+
+
+  if (!(Object.prototype.toString.apply(date) === '[object Date]')) throw new Error();
+
+  const data = date.getMonth();
+
+  if (data === 11 || data === 0 || data === 1) return 'winter';
+  if (data === 2 || data === 3 || data === 4) return 'spring';
+  if (data === 5 || data === 6 || data === 7) return 'summer';
+  if (data === 8 || data === 9 || data === 10) return 'autumn';
 };
