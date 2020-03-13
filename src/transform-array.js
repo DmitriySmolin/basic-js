8c3a3efdc61db8fd172701465baf9e965d2e8a32
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-03-13 14:09:56 +0400

    feat: add solution for task "transform-array"

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,44 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  let res = [];
+
+  let newArr = filtredArr(arr);
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      res.slice[i + 1];
+      continue;
+    }
+
+    if (arr[i] === "--discard-prev") {
+      res.pop();
+      continue;
+    }
+
+    if (arr[i] === "--double-next") {
+      res.push(arr[i + 1]);
+      continue;
+    }
+
+    if (arr[i] === "--double-prev") {
+      res.push(arr[i - 1]);
+      continue;
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
 };
+
+function filtredArr(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i]; j++) {
+      if (arr[i][j] == undefined) continue;
+    }
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
