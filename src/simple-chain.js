7ae79a602049477c496e360dbafcb1d3360a202f
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-03-13 14:11:21 +0400

    feat: add solution for task "simple-chain"

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,31 @@
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position > this.getLength() || position < 1) {
+      this.arr = [];
+      throw new Error();
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let res = this.arr.map(el => `( ${el} )`).join('~~');
+    this.arr = [];
+    return res;
   }
-};
+}
+
 
 module.exports = chainMaker;
