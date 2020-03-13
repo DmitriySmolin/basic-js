e7663c4d2801850b601f68e3d22a357a1f7e5528
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-03-13 14:13:18 +0400

    feat: add solution for task "extended-repeater"

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,53 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+    str = str + '';
+
+    let defaultOptions = {
+        repeatTimes: 1,
+        separator: '+',
+        addition: '',
+        additionRepeatTimes: 1,
+        additionSeparator: '|',
+    }
+
+    if (!options.repeatTimes) {
+        options.repeatTimes = defaultOptions.repeatTimes;
+    }
+
+    if (!options.separator) {
+        options.separator = defaultOptions.separator;
+    }
+
+    if (options.addition) {
+        options.addition = options.addition + '';
+    }
+    if (options.addition === undefined) {
+        options.addition = defaultOptions.addition;
+    }
+
+    if (!options.additionRepeatTimes) {
+        options.additionRepeatTimes = defaultOptions.additionRepeatTimes;
+    }
+
+    if (!options.additionSeparator) {
+        options.additionSeparator = defaultOptions.additionSeparator;
+    }
+
+    for (let i = 1; i <= options.repeatTimes; i++) {
+
+        result += str;
+
+        for (let j = 1; j <= options.additionRepeatTimes; j++) {
+
+            result += options.addition;
+
+            if (j !== options.additionRepeatTimes) result += options.additionSeparator;
+
+        }
+
+        if (i !== options.repeatTimes) result += options.separator;
+
+
+    }
+    return result;
 };
-  
