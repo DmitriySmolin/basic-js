e299cca0cba09dc07b20a7ade1a42e0b6bf1d0eb
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-03-13 14:14:29 +0400

    feat: add solution for task "vigenere cipher"

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,78 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(mode) {
+        this.mode = mode;
+        this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+
+        if (!message || !key) throw new Error();
+
+        let m = message.toUpperCase();
+        let k = key.toUpperCase();
+
+        let result = '';
+
+        for (let i = 0, j = 0; i < m.length; i += 1) {
+
+            if (!this.alphabet.includes(m[i])) {
+                result += m[i];
+                continue;
+            }
+
+            let resultIndex = this.alphabet.indexOf(m[i]) + this.alphabet.indexOf(k[j]);
+
+            if (this.alphabet.length - 1 - resultIndex < 0) {
+                resultIndex = Math.abs(this.alphabet.length - 1 - resultIndex) - 1;
+            } else {
+                resultIndex = resultIndex;
+            }
+
+            result += this.alphabet[resultIndex];
+
+            j += 1;
+            if (j === k.length) j = 0;
+        }
+
+        this.mode === false ? result = result.split('').reverse().join('') : result
+
+        return result;
+    }
+
+    decrypt(encryptedMessage, key) {
+
+        if (!encryptedMessage || !key) throw new Error();
+
+
+        let m = encryptedMessage.toUpperCase();
+        let k = key.toUpperCase();
+
+        let result = '';
+
+        for (let i = 0, j = 0; i < m.length; i += 1) {
+
+            if (!this.alphabet.includes(m[i])) {
+                result += m[i];
+                continue;
+            }
+
+            let mIdx = this.alphabet.indexOf(m[i]);
+            let kIdx = this.alphabet.indexOf(k[j]);
+
+            let resultIndex = 1 + (mIdx > kIdx) ? mIdx - kIdx : null;
+
+            if (resultIndex < 0) resultIndex = this.alphabet.length + resultIndex;
+
+            result += this.alphabet[resultIndex];
+
+            j += 1;
+            if (j === k.length) j = 0;
+        }
+
+        this.mode === false ? result = result.split('').reverse().join('') : result
+
+
+        return result;
     }
 }
 
